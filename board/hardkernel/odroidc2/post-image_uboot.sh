714f8a5ff1389eae57ad8eea541a5558a67c7996
Patrick Moran <pmoran1211@Patrick-Morans-MacBook-Pro.local>
2019-02-23 00:37:30 -0500

    first commit

--- board/hardkernel/odroidc2/post-image_uboot.sh
+++ board/hardkernel/odroidc2/post-image_uboot.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-mkdir -p "${BINARIES_DIR}/batocera-odroidc2-uboot" || exit 1
-cp "${BUILD_DIR}/uboot-odroidc2-v2015.01/sd_fuse/bl1.bin.hardkernel" "${BINARIES_DIR}/batocera-odroidc2-uboot" || exit 1
-cp "${BINARIES_DIR}/u-boot.bin" "${BINARIES_DIR}/batocera-odroidc2-uboot"                                      || exit 1
-(cd "${BINARIES_DIR}" && tar zcf batocera-odroidc2-uboot.tar.gz batocera-odroidc2-uboot)                       || exit 1
-rm -rf "${BINARIES_DIR}/batocera-odroidc2-uboot"                                                               || exit 1
+mkdir -p "${BINARIES_DIR}/gamestation-odroidc2-uboot" || exit 1
+cp "${BUILD_DIR}/uboot-odroidc2-v2015.01/sd_fuse/bl1.bin.hardkernel" "${BINARIES_DIR}/gamestation-odroidc2-uboot" || exit 1
+cp "${BINARIES_DIR}/u-boot.bin" "${BINARIES_DIR}/gamestation-odroidc2-uboot"                                      || exit 1
+(cd "${BINARIES_DIR}" && tar zcf gamestation-odroidc2-uboot.tar.gz gamestation-odroidc2-uboot)                       || exit 1
+rm -rf "${BINARIES_DIR}/gamestation-odroidc2-uboot"                                                               || exit 1
 
 exit 0
